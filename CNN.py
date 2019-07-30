4a633271e4e66f09258e55e276bbc49fca87f771
aasquier <aasquier@pdx.edu>
2019-07-29 20:54:14 -0700

    Added callbacks

--- CNN.py
+++ CNN.py
@@ -1,19 +1,12 @@
 import matplotlib as plt
-
 from SRData import load_dataset_from_hdf5
 from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
 
-# Keras packages model
-from keras.models import Model
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.layers import Input
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau 
+from keras.layers import Dense, Flatten, Dropout, Input
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.layers.merge import concatenate
+from keras.models import Model, Sequential
 from keras.optimizers import Adam
 from keras.utils import to_categorical
 
@@ -21,27 +14,27 @@ from keras.utils import to_categorical
 MODEL_FILE = 'Model.h5'
 SOURCE_DATA_FILE = 'SRData.h5'
 
-EPOCHS = 1000
-BATCH_SIZE = 33
-ADAM_LEARNING_RATE = 0.0001
-
+EPOCHS = 10
+BATCH_SIZE = 36
+ADAM_LEARNING_RATE = 0.00005
 VERBOSE = 1
+
 
 # load, shuffle, one-hot encode, and split the data up for the model
 def load_data():
     x_train, y_train = load_dataset_from_hdf5(SOURCE_DATA_FILE)
 
-    # split the data into training and test sets, 80/20
-    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2) 
+    # split the data into training and validation sets, 80/20
+    x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2) 
 
     # one-hot encode target values
     y_train = to_categorical(y_train)
-    y_test  = to_categorical(y_test)
+    y_valid = to_categorical(y_valid)
 
-    return x_train, x_test, y_train, y_test
+    return x_train, x_valid, y_train, y_valid
 
 
-def evaluate_model(x_train, x_test, y_train, y_test, load_model, save_model):
+def evaluate_model(x_train, x_valid, y_train, y_valid, load_model, show_plots):
     # load a saved Keras model
     if load_model:
         model = load_model(MODEL_FILE)
@@ -50,9 +43,8 @@ def evaluate_model(x_train, x_test, y_train, y_test, load_model, save_model):
         model=Sequential()
         # Input convolutional layer
         model.add(Conv1D(filters=32, kernel_size=7, activation='relu', input_shape=(n_timesteps, n_features)))
-        # # Second convolutional layer
+        # Second convolutional layer
         model.add(Conv1D(filters=32, kernel_size=7, activation='relu'))
-
         # Dropout layer, randomly removes p percentage of values, helps with overfitting
         model.add(Dropout(0.7))
         # Pooling layer to remove noise
@@ -60,23 +52,21 @@ def evaluate_model(x_train, x_test, y_train, y_test, load_model, save_model):
         # Flatten convolution out to attach to dense NN
         model.add(Flatten())
         # Hidden layer
-        model.add(Dense(200, activation='relu'))
+        model.add(Dense(100, activation='relu'))
         # Softmax output layer so that the the values are turned into probablities to assess
         model.add(Dense(n_outputs, activation='softmax'))
 
+        earlyStopping = EarlyStopping(monitor='val_loss', patience=100, verbose=0, mode='min')
+        mcp_save = ModelCheckpoint(MODEL_FILE, save_best_only=True, monitor='val_loss', mode='min')
+        # reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
         opt=Adam(lr=ADAM_LEARNING_RATE)
         model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-        model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
-
-    loss, accuracy = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
-
-    if save_model:
-        model.save(MODEL_FILE)
+        model.fit(x_train, y_train, validation_data=(x_valid, y_valid), callbacks=[earlyStopping, mcp_save], epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
 	
-    return loss, accuracy
+    return
 
 
-def evaluate_model_multihead(x_train, x_test, y_train, y_test, load_model, save_model):
+def evaluate_model_multihead(x_train, x_valid, y_train, y_valid, load_model, show_plots):
 # load a saved Keras model
     if load_model:
         model = load_model(MODEL_FILE)
@@ -84,19 +74,19 @@ def evaluate_model_multihead(x_train, x_test, y_train, y_test, load_model, save_
         n_timesteps, n_features, n_outputs = x_train.shape[1], x_train.shape[2], y_train.shape[1]
         # head 1
         inputs1 = Input(shape=(n_timesteps,n_features))
-        conv1 = Conv1D(filters=32, kernel_size=5, activation='relu')(inputs1)
+        conv1 = Conv1D(filters=32, kernel_size=3, activation='relu')(inputs1)
         drop1 = Dropout(0.7)(conv1)
         pool1 = MaxPooling1D(pool_size=2)(drop1)
         flat1 = Flatten()(pool1)
         # head 2
         inputs2 = Input(shape=(n_timesteps,n_features))
-        conv2 = Conv1D(filters=32, kernel_size=7, activation='relu')(inputs2)
+        conv2 = Conv1D(filters=32, kernel_size=5, activation='relu')(inputs2)
         drop2 = Dropout(0.7)(conv2)
         pool2 = MaxPooling1D(pool_size=2)(drop2)
         flat2 = Flatten()(pool2)
         # head 3
         inputs3 = Input(shape=(n_timesteps,n_features))
-        conv3 = Conv1D(filters=32, kernel_size=9, activation='relu')(inputs3)
+        conv3 = Conv1D(filters=32, kernel_size=7, activation='relu')(inputs3)
         drop3 = Dropout(0.7)(conv3)
         pool3 = MaxPooling1D(pool_size=2)(drop3)
         flat3 = Flatten()(pool3)
@@ -107,27 +97,23 @@ def evaluate_model_multihead(x_train, x_test, y_train, y_test, load_model, save_
         outputs = Dense(n_outputs, activation='softmax')(dense1)
         model = Model(inputs=[inputs1, inputs2, inputs3], outputs=outputs)
 
+        earlyStopping = EarlyStopping(monitor='val_loss', patience=100, verbose=0, mode='min')
+        mcp_save = ModelCheckpoint(MODEL_FILE, save_best_only=True, monitor='val_loss', mode='min')
+        # reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
         opt=Adam(lr=ADAM_LEARNING_RATE)
         model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-        model.fit([x_train, x_train, x_train], y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
-
-    loss, accuracy = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
-
-    if save_model:
-        model.save(MODEL_FILE)
+        model.fit([x_train,x_train,x_train], y_train, validation_data=(x_valid, y_valid), callbacks=[earlyStopping, mcp_save], epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
 	
-    return loss, accuracy 
+    return 
 
 
 def main():
-    
-    x_train, x_test, y_train, y_test = load_data()
-    _, acc = evaluate_model(x_train, x_test, y_train, y_test, False, False)
-    _, acc = evaluate_model_multihead(x_train, x_test, y_train, y_test, False, False)
-
-    print(f'Accuracy of model: {acc}')
+    x_train, x_valid, y_train, y_valid = load_data()
+    evaluate_model(x_train, x_valid, y_train, y_valid, False, False)
+    # evaluate_model_multihead(x_train, x_test, y_train, y_test, False, False)
 
     return
+
 
 
 main()
